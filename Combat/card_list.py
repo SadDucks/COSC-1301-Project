5030041b628f85ad40dc46e5f7f87089edd6d058
BossBryce0704 <Brycecandrews@gmail.com>
2026-09-14 20:55:58 -0500

    Removed Unnecessary Code

--- Combat/card_list.py
+++ Combat/card_list.py
@@ -31,13 +31,3 @@ for card in supportCards:
     )
     supportCardList.append(newCard)
 
-
-
-
-#Link = attackCard("Link", "IMAGE", 15, 5, 50)
-#Gannondorf = attackCard("Gannondorf", "IMAGE", 15, 5, 50)
-#Zelda = attackCard("Zelda", "IMAGE", 20, 5, 25)
-#Calamo = attackCard("Calamo", "IMAGE", 5, 2, 10)
-
-#healing_potion = supportCard("Healing Potion", "IMAGE", 0, 25, 3)
-#energy_potion = supportCard("Energy Potion", "IMAGE", 0, 0, -5)
