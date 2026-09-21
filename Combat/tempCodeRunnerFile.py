22eb0c823711eb00fd921ac4224fe4ffe942e1db
SadDucks <onionpupz@gmail.com>
2026-09-20 21:13:25 -0500

    Removed uncessarry files

--- Combat/tempCodeRunnerFile.py
+++
@@ -1,8 +0,0 @@
-class card:
-    # Creates the basic information for every card
-    def __init__(self, name, image):
-        self.name = name
-        self.image = image
-
-    def __str__(self):
-        return self.name
